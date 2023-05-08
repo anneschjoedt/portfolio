87cb193720fc30aac921720782b919546ff7b1c0
anneschjoedt <anne.schjoedt@gmail.com>
2023-05-08 17:27:43 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages";
 import Contact from "./pages/ContactPage";
 import "./fonts/wotfard-regular-webfont.ttf";
@@ -12,14 +12,14 @@ import AboutPage from "./pages/AboutPage";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/contact" component={Contact} exact />
-        <Route path="/jobspire" component={JobspireProject} exact />
-        <Route path="/annoanno" component={AnnoAnnoProject} exact />
-        <Route path="/aben" component={AbenProject} exact />
-        <Route path="/about" component={AboutPage} exact />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/contact" element={<Contact />} exact />
+        <Route path="/jobspire" element={<JobspireProject />} exact />
+        <Route path="/annoanno" element={<AnnoAnnoProject />} exact />
+        <Route path="/aben" element={<AbenProject />} exact />
+        <Route path="/about" element={<AboutPage />} exact />
+      </Routes>
     </Router>
   );
 }
