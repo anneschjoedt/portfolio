e98e3999d5319fffa653e88ba218197cf03a1ce4
anneschjoedt <anne.schjoedt@gmail.com>
2023-06-12 12:22:52 +0200

    cursor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import AnimatedCursor from "react-animated-cursor";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -8,5 +9,6 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <AnimatedCursor />
   </StrictMode>
 );
